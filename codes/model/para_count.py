58fddf3efda57b4280846fb0309839e97418b42a
Xuezi Gong <139324704+XueziGong@users.noreply.github.com>
2025-06-11 17:31:48 +0800

    Update para_count.py

--- codes/model/para_count.py
+++ codes/model/para_count.py
@@ -1,5 +1,3 @@
-import os
-os.chdir('.../KLE-PINN-EC/codes') # replace with the correct path
 import torch
 import torch.nn as nn
 import numpy as np
@@ -8,19 +6,18 @@ from model import kle_pinn_ec
 
 DEVICE = 'cpu'
 
+# prior
 mu_lnKs = 3.0
 eta = 50
 L = 100
 variance = 3
-nkl = 9
-
-# initialize the model
 nkl = 9
 eigen_info = 'eta=' + str(eta) + '_var=' + str(variance) + '/'
 eigen_path = '.../KLE-PINN-EC/data/eigens/' + eigen_info # replace with the correct path
 wn_np = np.load(eigen_path + 'selected_wn.npy') # selected positive roots
 wn = torch.tensor(wn_np, dtype=torch.float32).to(DEVICE)
 
+# initialize the model
 model = kle_pinn_ec.pinn(nkl, eta, variance, wn, L, DEVICE).to(DEVICE)
 print(model)
 
